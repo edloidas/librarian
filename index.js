de0401c42422168015d76b493082bf654ffa0c66
Mikita Taukachou <edloidas@gmail.com>
2017-04-21 11:37:32 +0300

    Remove comments

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-// Require libraries.
 const botkit = require('botkit');
 const { token } = require('./src/config');
 
-// Create slackbot controller.
 const controller = botkit.slackbot({
   debug: false,
   send_via_rtm: true,
   json_file_store: './db',
 });
 
-// Create the slackbot.
 const slackBot = controller.spawn({ token });
 
-// Listen for a direct message.
 controller.hears([/^hello robot$/i], ['direct_message'], (bot, message) => {
   bot.reply(message, 'Hello Human!');
 });
 
-// Start real-time messaging.
 slackBot.startRTM();
